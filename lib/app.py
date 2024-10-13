5c596887cdf992b1f3b4cdc029ea72dfa7fa89e6
Reagan-Ongaya <reagan.ongaya@student.moringaschool.com>
2024-10-13 12:59:28 +0300

    simple python print() function

---
+++ lib/app.py
@@ -0,0 +1,6 @@
+#basic python code 
+#pyhton uses print() to out put .ie "datatypes"
+
+print("hello World !", end=" ")
+print("My name is Reagan Jake", end=" ")
+print=("I love programming ......")
